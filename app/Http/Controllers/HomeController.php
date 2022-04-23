3b73b8cdb5c94cf76820957d773c986bc4868f82
RobertDocker <robert.comunicar@gmail.com>
2022-04-23 15:19:14 -0300

    Spatie - Configurado

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        // Cria uma função
+        //$role = Role::create(['name' => 'writer']);
+
+        // Cria uma permissão
+        //$permission = Permission::create(['name' => 'create articles']);
+
+        // Pesquisa uma função pelo id
+        //$role = Role::findById(1);
+
+        // Pesquisa uma permissão pelo id
+        //$permission = Permission::findById(1);
+
+        // Uma função recebe uma permissão
+        //$role->givePermissionTo($permission);
+
+        // Uma permissão se liga a uma função
+        //$permission->assignRole($role);
+
+        //auth()->user()->givePermissionTo("write posts");
+        //auth()->user()->assignRole("writer");
+
+        return auth()->user()->permissions;
+
+        //return view("welcome");
+    }
+}
